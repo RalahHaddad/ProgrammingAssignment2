a33391178c4a5199545002f832fd5db3ad15efb0
RalahHaddad <ralah.haddad@gmail.com>
2015-01-24 16:40:43 +0400

    This is m submittion for the second programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## function creates the appropriate data structure for the caching matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        reverse <- NULL
+        set <- function(y) {
+                x <<- y
+                reverse <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) reverse <<- solve
+        getsolve <- function() reverse
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## function inverse the input matrix using the cahcin mechanism
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        reverse <- x$getsolve()
+        if(!is.null(reverse)) {
+                message("getting cached data")
+                return(reverse)
+        }
+        data <- x$get()
+        reverse <- solve(data, ...)
+        x$setsolve(reverse)
+        reverse
 }
+
